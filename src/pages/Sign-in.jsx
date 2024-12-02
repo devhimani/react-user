8a91509aed3a3cecd23a9d9ab1422451b6419234
himanigupta <gupta.himani44@gmail.com>
2024-12-01 22:11:13 -0800

    get form data in signup & signin

--- src/pages/Sign-in.jsx
+++ src/pages/Sign-in.jsx
@@ -1,14 +1,50 @@
 import { Link } from "react-router-dom";
 import "./Sign-in.css";
+import { useState } from "react";
 const SignIn = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleSignin = (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      setError("All fields are required");
+      return;
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+    setError("");
+  };
+
   return (
     <div className="signin_container">
       <h1> Sign-in </h1>
-      <form className="signin_form">
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="password" />
+      <form onSubmit={handleSignin} className="signin_form">
+        <input
+          type="text"
+          placeholder="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
         <button type="submit">Sign In</button>
       </form>
+      {error && <p className="error"> {error} </p>}
       <p>
         Do not have an acoount? <Link to={"/sign-up"}> Sign Up</Link>
       </p>
--- src/pages/Sign-up.jsx
+++ src/pages/Sign-up.jsx
@@ -1,16 +1,77 @@
 import { Link } from "react-router-dom";
 import "./Sign-up.css";
+import { useState } from "react";
+
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handlechange = (e) => {
+    setError("");
+
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+
+    if (
+      !formData.name ||
+      !formData.email ||
+      !formData.password ||
+      !formData.confirmpassword
+    ) {
+      setError("All fileds are required");
+      return;
+    }
+
+    if (formData.password !== formData.confirmpassword) {
+      setError("Password didn't match");
+    }
+  };
+
   return (
     <div className="signup_container">
       <h1>Sign-up</h1>
-      <form className="signup_form">
-        <input type="text" placeholder="name" />
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="password" />
-        <input type="text" placeholder="confirm password" />
+      <form onSubmit={handleSignUp} className="signup_form">
+        <input
+          type="text"
+          placeholder="name"
+          name="name"
+          value={formData.name}
+          onChange={handlechange}
+        />
+        <input
+          type="text"
+          placeholder="email"
+          name="email"
+          value={formData.email}
+          onChange={handlechange}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          name="password"
+          value={formData.password}
+          onChange={handlechange}
+        />
+        <input
+          type="text"
+          placeholder="confirm password"
+          name="confirmpassword"
+          value={formData.confirmpassword}
+          onChange={handlechange}
+        />
         <button type="submit">Sign Up</button>
       </form>
+      {error && <p className="error">{error}</p>}
       <p>
         Already have an account? <Link to={"/sign-in"}>Sign In</Link>
       </p>
