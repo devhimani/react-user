a5e08eff40bb4c573352dd9a72f3c7a20826f919
himanigupta <gupta.himani44@gmail.com>
2024-12-02 23:18:22 -0800

    add context

---
+++ src/store/authContext.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+export const authContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [users, setUsers] = useState([]);
+  return (
+    <authContext.Provider value={{ users, setUsers }}>
+      {children}
+    </authContext.Provider>
+  );
+};
+export default AuthProvider;
