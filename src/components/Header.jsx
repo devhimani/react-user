69d885695af5fd54533c99a0374e2910608fc4aa
himanigupta <gupta.himani44@gmail.com>
2024-12-03 23:34:10 -0800

    currentUser Sigin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
+import { useContext } from "react";
+import { authContext } from "../store/authContext";
 
 const Header = () => {
+  const { currentUser } = useContext(authContext);
   return (
     <div className="header">
       <h1> Header-Icon</h1>
       <div>
-        <Link to={"/sign-in"}>
-          <button>Signin</button>
-        </Link>
+        {currentUser ? (
+          <p> {currentUser.name}</p>
+        ) : (
+          <Link to={"/sign-in"}>
+            <button>Signin</button>
+          </Link>
+        )}
       </div>
     </div>
   );
